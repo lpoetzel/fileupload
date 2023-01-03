45fe4eb8c42a57e3d4b30de4106c733e6757874a
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-03 15:04:46 +0100

    axios + handlesubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [file, setFile] = useState();
+
+  function handleChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const url = "http://localhost:3000/uploadFile";
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("filename", file.name);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    axios.post(url, formData, config).then((response) => {
+      console.log(response.data);
+    });
+  }
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>React File Upload</h1>
-        <input type="file" />
+        <input type="file" onChange={handleChange} />
         <button type="submit">Upload</button>
       </form>
     </div>
