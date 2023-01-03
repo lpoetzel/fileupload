91cd40671c9deeace400af3e589de3ea41148b8c
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-03 15:43:42 +0100

    include fs for viewable renaming

--- back/server.js
+++ back/server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/uploadFile", upload.single("file"), uploadFiles);
+app.post("/uploadFile", upload.single("file"), (req, res) => {
+  const { originalname } = req.file;
 
-function uploadFiles(req, res) {
-  console.log(req.body);
-  console.log(req.file);
-  res.json({ message: "Successfully uploaded file" });
-}
+  const { filename } = req.file;
+
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+
+      res.send("File uploaded");
+    }
+  );
+});
 app.listen(5000, () => {
   console.log(`Server started...`);
 });
